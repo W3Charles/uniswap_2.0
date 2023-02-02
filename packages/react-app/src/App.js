814727125dc2b4abfc853118ade9adb62ead80b4
W3Charles <www3charles@gmail.com>
2023-02-02 04:46:10 -0500

    build: import useEthers, styles and uniswapLogo  in App js

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,4 +1,8 @@
 import React from "react"
+import { useEthers } from '@usedapp/core';
+
+import styles from './styles';
+import { uniswapLogo } from './assets';
 
 const App = () => {
   return <p>Hello, Web3!</p>
