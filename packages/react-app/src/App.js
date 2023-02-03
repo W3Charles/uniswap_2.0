077334e7c1b113b44e88c8631f725616b45a7a7c
W3Charles <www3charles@gmail.com>
2023-02-03 04:23:38 -0500

    feat: add app header & sub title

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -17,6 +17,10 @@ const App = () => {
           WalletButton
         </header>
         
+        <div className={styles.exchangeContainer}>
+          <h1 className={styles.headTitle}>Uniswap 2.0</h1>
+          <p className={styles.subTitle}>Exchange tokens in seconds</p>
+        </div>
       </div>
     </div>
   )
