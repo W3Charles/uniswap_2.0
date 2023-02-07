b8e87366cf49c6321c4558f60e86b120c1f481ec
W3Charles <www3charles@gmail.com>
2023-02-07 02:19:00 -0500

    feat: add dynamic button w/ onClick fuction
    - If rendered equals to an empty string, show Connect Wallet
    
    - If rendered is not equal to an empty string, then show rendered (acct address)

--- packages/react-app/src/components/WalletButton.js
+++ packages/react-app/src/components/WalletButton.js
@@ -2,14 +2,28 @@ import React, {useState, useEffect} from 'react';
 import {shortenAddress, useEthers, useLookupAddress} from "@usedapp/core";
 
 import styles from "../styles";
+import { render } from 'react-dom';
 
 const WalletButton = () => {
   const [rendered, setRendered] = useState("");
-  
+
   const {ens} = useLookupAddress();
   const {account, activateBrowserWallet, deactivate} = useEthers();
+  
   return (
-    <div>WalletButton</div>
+    <button
+      onClick={() => {
+        if (!account) {
+          activateBrowserWallet();
+        } else {
+          deactivate();
+        }
+      }}
+      className={styles.walletButton}
+    >
+      {rendered === "" && "Connect Wallet"}
+      {rendered !== "" && rendered}
+    </button>
   )
 }
 
