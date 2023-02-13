6d8ffa0b97637dabb628895346ae7107f3829bb8
W3Charles <www3charles@gmail.com>
2023-02-13 04:22:32 -0500

    build: remove react-dom import

--- packages/react-app/src/components/WalletButton.js
+++ packages/react-app/src/components/WalletButton.js
@@ -1,6 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom'
-
 import {shortenAddress, useEthers, useLookupAddress} from '@usedapp/core';
 
 import styles from '../styles';
