a3c6a7ae027f839d1b8d45e2676f3db064eb38b1
W3Charles <www3charles@gmail.com>
2023-02-20 05:45:32 -0500

    build: add router address & provider url to config

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = "0x6888dcd3050d7d0cd97685178d82d000a34652cde31484a7abb867797695df4c"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/2rIwMR8DX0SxVV4QdV-04Ipaa-cTsw2T",
   },
 };
