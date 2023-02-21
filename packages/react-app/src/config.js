8befcdb31acaa89be0cea2111810444ae0ac9126
W3Charles <www3charles@gmail.com>
2023-02-21 04:03:47 -0500

    style: add comment to config.js
    - Add router address to env file before you deploy to Mainnet

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,5 +1,6 @@
 import { Goerli } from "@usedapp/core";
 
+/* Put this in an env file BEFORE you deploy on mainnet */
 export const ROUTER_ADDRESS = "0x61b7fb401c4b3d7401df9e94193a7575f4ede526"; 
 
 export const DAPP_CONFIG = {
