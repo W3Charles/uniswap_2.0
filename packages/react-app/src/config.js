a4f56a8690e97cdf93635958255f07a108a010cb
W3Charles <www3charles@gmail.com>
2023-02-21 02:17:44 -0500

    build: update router address in config file

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,6 +1,6 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "0x6888dcd3050d7d0cd97685178d82d000a34652cde31484a7abb867797695df4c"; 
+export const ROUTER_ADDRESS = "0x61b7fb401c4b3d7401df9e94193a7575f4ede526"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
